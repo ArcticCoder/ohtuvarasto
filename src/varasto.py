0cec684b4eafdff9cc40af950ead98af61286d64
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-11-12 15:21:58 +0200

    Break pylint

--- src/varasto.py
+++ src/varasto.py
@@ -9,8 +9,7 @@ class Varasto:
             self.saldo = min(alku_saldo, tilavuus)
 
 
-    # huom: ominaisuus voidaan myös laskea.
-    # Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
